b04de7de1eb5bde7728e3047573eeba9df0af744
nomid03 <nomd2005@gmail.com>
2022-11-10 10:45:12 +0100

    instal node and make function

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const harina = ""
+const harina = "harina for ";
+const agua = "Agua";
+const aguaTemplada="agua temmplada "
+const aceite ="Aceite";
+const sal ="sla";
+const levadura= "levaduara de panaderia";
+const azucar="azucar";
+const salsa="salsa de tomte ";
+const queso="man";
+const pepperoni="pepperoni";
+const aceitunas="accetunas";
+const championones="championones";
+
+let harinaPeso="400 grs";
+let AguaPeso="100 grs";
+let AceitePeso="50 grs";
+let salPeso="1 cuucharadita de cafe";
+let levaduraPeso="16 grs";
+let azucarPeso="1 cuucharadita de  cafe ";
+function prepararLevadura(){
+    console.log("Mezclar el" + aguaTemplada + ",el " +azucar+ "y la "+ levadura+"," );
+
+}
+prepararLevadura();
